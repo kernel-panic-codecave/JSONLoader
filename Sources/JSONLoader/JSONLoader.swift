4a605d292a3c83fef069063f9a6aa4ca2b1b6b65
DrBeta <freedom>
2019-08-03 09:46:04 -0500

    I think this will fix the framework

--- Sources/JSONLoader/JSONLoader.swift
+++ Sources/JSONLoader/JSONLoader.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+class Loader{
 public func load<T: Decodable>(_ filename: String, as type:T.Type = T.self) -> T {
     let data:Data
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
@@ -19,4 +20,5 @@ public func load<T: Decodable>(_ filename: String, as type:T.Type = T.self) -> T
     } catch {
         fatalError("Couldn't parse \(filename) as \(T.self)\n\(error)")
     }
+}
 }
