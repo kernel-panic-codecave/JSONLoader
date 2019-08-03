f3143aa12641b79416a349b9adfdddd03881563e
DrBeta <freedom>
2019-08-03 09:40:08 -0500

    Maybe this will let me use my new framework for loading JSON

--- Sources/JSONLoader/JSONLoader.swift
+++ Sources/JSONLoader/JSONLoader.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func load<T: Decodable>(_ filename: String, as type:T.Type = T.self) -> T {
+public func load<T: Decodable>(_ filename: String, as type:T.Type = T.self) -> T {
     let data:Data
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
         else {
